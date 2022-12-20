d31209eb464e84e190fb383ca2179176be357663
Thomas <t.kudin@generals-soft.com>
2022-12-20 17:23:46 +0300

    I did svg icon for table

--- src/components/Cost/TableHeader/TableHeader.jsx
+++ src/components/Cost/TableHeader/TableHeader.jsx
@@ -1,12 +1,12 @@
 import classes from "./table-header.module.css"
 
 const TableHeader = ({ title, handlerToggle, isOpen }) => {
-   return (
-      <div className={classes.general__list}>
-         <h2>{title}</h2>
-         <button onClick={handlerToggle}>svg {isOpen ? "close" : "open"}</button>
-      </div>
-   )
+  return (
+    <div className={classes.general__list}>
+      <h2>{title}</h2>
+      <div className={classes.general__btn} onClick={handlerToggle}><div className={`${classes.svg__icon} ${isOpen ? classes.active : ""}`}></div></div>
+    </div>
+  )
 }
 
 export default TableHeader
