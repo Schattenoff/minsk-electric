885571f4bd83fbe9865babdbbe0674c7090636c6
Thomas <t.kudin@generals-soft.com>
2022-12-20 13:58:37 +0300

    change cost__table

--- src/components/Cost/Table/TableRow/TableRow.jsx
+++ src/components/Cost/Table/TableRow/TableRow.jsx
@@ -2,31 +2,33 @@ import { useState } from "react"
 import classes from "./table-row.module.css"
 
 const TableRow = ({ item, checked, onChangeChecked, amount, checkedList }) => {
-   const [totalAmount, setTotalAmount] = useState(checked ? amount : 0)
+  const [totalAmount, setTotalAmount] = useState(checked ? amount : 0)
 
-   const handlerAmountChange = (e) => {
-      setTotalAmount(e.target.value)
-      checkedList[checkedList.findIndex(listItem => listItem.id === item.id)].value = +(e.target.value * item.cost).toFixed(2)
-   }
+  const handlerAmountChange = (e) => {
+    if (e.target.value < 0) {
+      return e.target.value = "";
+    }
+    setTotalAmount(e.target.value)
+    checkedList[checkedList.findIndex(listItem => listItem.id === item.id)].value = +(e.target.value * item.cost).toFixed(2)
+  }
 
-   return (
-      <tr className={checked ? classes.active : ""}>
-         <td><input type={"checkbox"} checked={checked} onChange={onChangeChecked} /></td>
-         <td>{item.name}</td>
-         <td>{item.measure}</td>
-         <td>{item.cost}</td>
-         <td className={classes.cost__table_amount}>
-            <input
-               type="number"
-               min="0"
-               value={totalAmount}
-               className={checked ? classes.visible : classes.hidden}
-               onChange={handlerAmountChange}
-            />
-         </td>
-         <td>{(totalAmount * item.cost).toFixed(2)}</td>
-      </tr>
-   )
+  return (
+    <tr className={checked ? classes.active : ""}>
+      <td><input type={"checkbox"} checked={checked} onChange={onChangeChecked} /></td>
+      <td>{item.name}</td>
+      <td className={classes.cost__table}>{item.measure}</td>
+      <td className={classes.cost__table}>{item.cost}</td>
+      <td className={`${classes.cost__table} ${classes.amount}`}>
+        {checked ? <input
+          type="number"
+          value={totalAmount}
+          onChange={handlerAmountChange}
+        /> : <p>0</p>}
+
+      </td>
+      <td className={classes.cost__table}>{(totalAmount * item.cost).toFixed(2)}</td>
+    </tr >
+  )
 }
 
 export default TableRow
